7605c20c0b40befb3ac464d6e39383ac56db141f
Boniface Mwenda <bonny90.m@gmail.com>
2016-10-08 18:15:53 +0300

    Added a docstring to fibonacci function

--- fibonacci_numbers.py
+++ fibonacci_numbers.py
@@ -1,11 +1,11 @@
-'''Fibonacci numbers of the 1st n terms'''
-
-
 def fibonacci(n):
+    '''Get the fibonacci numbers of the 1st n terms'''
+
     # Initialize first two fibonacci numbers
     fib1 = 0
     fib2 = 1
     lst = []
+    # Add the last number and second last number
     for i in range(n):
         lst.append(fib1)
         nextNum = fib1 + fib2
