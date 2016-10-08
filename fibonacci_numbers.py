5d98d6a226ebddb9d8f3c19f45290c571df063b8
Boniface Mwenda <bonny90.m@gmail.com>
2016-10-08 19:38:19 +0300

    Added input validation conditions

--- fibonacci_numbers.py
+++ fibonacci_numbers.py
@@ -4,10 +4,19 @@
 def fibonacci(n):
     '''Get fibonacci numbers of the 1st n terms'''
 
+    # Check that the input is valid
+    if type(n) is not int:
+        raise TypeError("Value must be integer")
+    elif n == 0:
+        return []
+    elif n < 0:
+        raise ValueError("Value must be greater than zero")
+
     # Initialize first two fibonacci numbers
     fib1 = 0
     fib2 = 1
     lst = []
+
     # Add the last number and second last number
     for i in range(n):
         lst.append(fib1)
@@ -15,3 +24,4 @@ def fibonacci(n):
         fib2 = fib1
         fib1 = nextNum
     return(lst)
+
