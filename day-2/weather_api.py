141e7c297d1112c5385a60f9d32b54c5fbaacac5
Boniface Mwenda <bonny90.m@gmail.com>
2016-10-11 15:15:12 +0300

    Get weather forecast from OpenWeather API

--- day-2/weather_api.py
+++ day-2/weather_api.py
@@ -1,11 +1,15 @@
 import requests
 
-# Send a GET request to OpenWeatherMap Api
-response = requests.get("http://api.openweathermap.org/data/2.5/forecast?id=192950&APPID=cc014ad6c4c7e0077ab2617434b2eabe")
 
+def get_weather():
+    """Get weather forecast from OpenWeatherMap"""
 
-if response.status_code != 200:
-    raise Exception(
-        "API error. Response status: {}".format(response.status_code))
-else:
-    print(response.json())
+    # Send a GET request
+    response = requests.get(
+        "http://api.openweathermap.org/data/2.5/forecast?id=192950&APPID=cc014ad6c4c7e0077ab2617434b2eabe")
+
+    if response.status_code != 200:
+        raise Exception(
+            "API error. Response status: {}".format(response.status_code))
+    else:
+        return response.json()
