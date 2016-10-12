35095adea095d409254e9ada229815dd0160abbc
Boniface Mwenda <bonny90.m@gmail.com>
2016-10-12 19:09:43 +0300

    Added docstring for get_primes function

---
+++ clinic/prime_numbers.py
@@ -0,0 +1,35 @@
+'''A function that gets a list of prime numbers'''
+
+
+def get_primes(n):
+    '''Get a list of first n prime numbers'''
+
+    # Check that the input is valid
+    if type(n) is not int:
+        raise TypeError("Value must be integer")
+    elif n == 0:
+        return []
+    elif n < 0:
+        raise ValueError("Value must be greater than zero")
+
+    # Create a list with the first prime number
+    lst = [2]
+    # Test each number for primeness, starting with 3
+    testNum = 3
+
+    while True:
+        prime = True
+        # Check if number under test is prime
+        for item in lst:
+            if testNum % item == 0:
+                prime = False
+                break
+                testNum += 1
+        if prime:
+            lst.append(testNum)
+        testNum += 1
+
+        # Break if len(lst) == n
+        if len(lst) == n:
+            break
+    return lst
